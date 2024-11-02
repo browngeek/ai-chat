a85c71cd29a7505ed6c442c13a12125972a6d997
sri varadarajan <browngeek@gmail.com>
2024-11-02 11:10:55 -0700

    v1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from'react-router-dom';
+import HomePage from './routes/HomePage/HomePage';
+import DashboardPage from './routes/DashboardPage/DashboardPage';
+import DashboardLayout from './layouts/dashboardLayout/DashboardLayout';
+import RootLayout from './layouts/rootLayout/RootLayout';
+
+import ChatPage from './routes/ChatPage/ChatPage';
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout/>,  
+    children : [
+      {
+        path: "/",
+        element: <HomePage/>, 
+      },
+      {
+        element: <DashboardLayout/>, 
+        children : [
+          {
+            path: "/dashboard",
+            element: <DashboardPage/>, 
+          },
+          {
+            path: "/dashboard/chats/:id",
+            element: <ChatPage/>,  // Route with dynamic parameter
+          }
+        ]
+      }
+  ]
+  },
+  
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
